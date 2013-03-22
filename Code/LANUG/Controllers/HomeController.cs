2cc2641bb65c6bf647f8c892cfdb8bd9dd773e94
Chris Yokley <cyokley@onstart.net>
2013-03-21 23:32:06 -0500

    Added contact form to contact page.

--- Code/LANUG/Controllers/HomeController.cs
+++ Code/LANUG/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using LANUG.Models;
+using System.Net.Mail;
+using System.Configuration;
 
 namespace LANUG.Controllers
 {
@@ -23,7 +25,40 @@ namespace LANUG.Controllers
 
         public ActionResult Contact()
         {
+            Random rand = new Random();
+            int v1 = rand.Next(1, 15);
+            int v2 = rand.Next(1, 15);
+            ViewBag.Captcha = string.Format("{0} + {1}", v1, v2);
+            ViewBag.CV = v1 + v2 + 108;
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult Contact(string Name, string Email, string Subject, string Message, string Captcha, int CV)
+        {
+            string rval = string.Empty;
+            try
+            {
+                int cval = 0;
+                int.TryParse(Captcha, out cval);
+                if (cval == CV - 108)
+                {
+                    MailMessage msg = new MailMessage(Email, ConfigurationManager.AppSettings["ContactEmail"]);
+                    msg.Subject = "LANUG Contact Submission: " + Subject;
+                    msg.IsBodyHtml = true;
+                    msg.Body = "New Message From " + Name + "<br/><br/>" + Message;
+
+                    SmtpClient smtp = new SmtpClient(ConfigurationManager.AppSettings["SmtpServer"]);
+                    smtp.Credentials = new System.Net.NetworkCredential(ConfigurationManager.AppSettings["SmtpUser"], ConfigurationManager.AppSettings["SmtpPassword"]);
+                    smtp.Send(msg);
+                }
+                else rval = "The captcha answer is incorrect.";
+            }
+            catch (Exception ex)
+            {
+                rval = ex.Message;
+            }
+            return Json(rval, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult Sponsors()
