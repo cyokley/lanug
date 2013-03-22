269673f4c70a80570e0ec595d7eeeeb764681992
Chris Yokley <cyokley@onstart.net>
2013-03-22 16:16:19 -0500

    Fixed jQuery Validation.

--- Code/LANUG/App_Start/BundleConfig.cs
+++ Code/LANUG/App_Start/BundleConfig.cs
@@ -15,8 +15,7 @@ namespace LANUG
                         "~/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
